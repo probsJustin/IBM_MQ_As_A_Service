71852801d5c2e86b8b17ae4152c8c4114a617c2b
Justin Hagerty <wheelsmanx@gmail.com>
2021-09-09 11:06:50 -0400

    add runnable mq rec examples - will need to be rewritten

--- src/main/java/com/probsjustin/IBMMQAAS/runnable_mq_rec.java
+++ src/main/java/com/probsjustin/IBMMQAAS/runnable_mq_rec.java
@@ -1,16 +1,62 @@
 package com.probsjustin.IBMMQAAS;
 
+import javax.jms.JMSException;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import com.ibm.mq.jms.MQQueue;
+import com.ibm.mq.jms.MQQueueConnection;
+import com.ibm.mq.jms.MQQueueConnectionFactory;
+import com.ibm.mq.jms.MQQueueReceiver;
+import com.ibm.mq.jms.MQQueueSession;
+import com.ibm.msg.client.wmq.WMQConstants;
+
 public class runnable_mq_rec {
 
-	logger_internal instance_logger_internal = new logger_internal(); 
+    logger_internal instance_logger_internal = new logger_internal();
 
-	
-	runnable_mq_rec(){
-		
-	}
-	
-	void run() {
-		
-	}
-	
+
+    runnable_mq_rec() {
+
+    }
+
+    void run() {
+
+        try {
+            MQQueueConnectionFactory cf = new MQQueueConnectionFactory();
+            cf.setHostName("localhost");
+            cf.setPort(1414);
+
+            cf.setIntProperty(WMQConstants.WMQ_CONNECTION_MODE, WMQConstants.WMQ_CM_CLIENT);
+
+            cf.setQueueManager("QM_GRIDSERVER");
+            cf.setChannel("SYSTEM.ADMIN.SVRCONN");
+
+            MQQueueConnection connection = (MQQueueConnection) cf.createQueueConnection();
+            //MQQueueConnection connection = (MQQueueConnection) cf.createQueueConnection("username","password");
+
+            MQQueueSession session = (MQQueueSession) connection.createQueueSession(false, Session.AUTO_ACKNOWLEDGE);
+
+            MQQueue queue = (MQQueue) session.createQueue("queue:///MyTestQueue");
+
+            MQQueueReceiver receiver = (MQQueueReceiver) session.createReceiver(queue);
+
+            connection.start();
+
+            TextMessage receivedMessage = (TextMessage) receiver.receive();
+            System.out.println("Received message from Queue MyTestQueue: " + receivedMessage.getText());
+
+            receiver.close();
+            session.close();
+            connection.close();
+            System.out.println("Message Received OK.\n");
+        } catch (JMSException jmsex) {
+            System.out.println(jmsex);
+            System.out.println("Message Receive Failure\n");
+        } catch (Exception ex) {
+            System.out.println(ex);
+            System.out.println("Message Receive Failure\n");
+        }
+    }
+
+
 }
